c814918dd7aa94e372237c62f09d5ced767b0a77
daniel shelton <danielshelton90@gmail.com>
2021-05-21 15:47:46 -0700

    created user signup route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # this is all the routes
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resource :users, except: [:new]
 end
